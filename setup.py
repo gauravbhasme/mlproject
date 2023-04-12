c3227968e563f43d83f85eb3ec95ddfe55c83623
gaurav <gauravsb1998@gmail.com>
2023-04-12 13:15:01 +0200

    first

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+#meta data of project
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this fun will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+            
+    return requirements
+    
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='gb',
+    author_email='gauravsb1998@gmail.com',
+    packages=find_packages(),
+    # install_requires=['pandas', 'numpy', 'seaborn']
+    install_requires=get_requirements('requirements.txt')
+    
+    
+)
+
